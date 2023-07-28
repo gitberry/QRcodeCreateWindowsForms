2f9a917fcf812ff5fe0988b888418340577ad934
C. Rensberry <gitberry@northberry.ca>
2023-07-27 21:29:46 -0600

    Add project files.

---
+++ frmQRCreate.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace QRCreate
+{
+    partial class QRCreate
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnRender = new System.Windows.Forms.Button();
+            this.txtTextToUse = new System.Windows.Forms.TextBox();
+            this.imgToRender = new System.Windows.Forms.PictureBox();
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.imgToRender)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRender
+            // 
+            this.btnRender.Location = new System.Drawing.Point(12, 12);
+            this.btnRender.Name = "btnRender";
+            this.btnRender.Size = new System.Drawing.Size(75, 23);
+            this.btnRender.TabIndex = 1;
+            this.btnRender.Text = "QR this!";
+            this.toolTip2.SetToolTip(this.btnRender, "Click on image below to copy image to clipboard");
+            this.btnRender.UseVisualStyleBackColor = true;
+            this.btnRender.Click += new System.EventHandler(this.btnRender_Click);
+            // 
+            // txtTextToUse
+            // 
+            this.txtTextToUse.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTextToUse.Location = new System.Drawing.Point(93, 13);
+            this.txtTextToUse.Name = "txtTextToUse";
+            this.txtTextToUse.Size = new System.Drawing.Size(686, 23);
+            this.txtTextToUse.TabIndex = 2;
+            this.toolTip2.SetToolTip(this.txtTextToUse, "Click on image below to copy to windows clipboard..");
+            // 
+            // imgToRender
+            // 
+            this.imgToRender.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.imgToRender.Location = new System.Drawing.Point(12, 41);
+            this.imgToRender.Name = "imgToRender";
+            this.imgToRender.Size = new System.Drawing.Size(768, 604);
+            this.imgToRender.TabIndex = 3;
+            this.imgToRender.TabStop = false;
+            this.imgToRender.Visible = false;
+            this.imgToRender.MouseClick += new System.Windows.Forms.MouseEventHandler(this.imgToRender_MouseClick);
+            // 
+            // QRCreate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 657);
+            this.Controls.Add(this.imgToRender);
+            this.Controls.Add(this.txtTextToUse);
+            this.Controls.Add(this.btnRender);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "QRCreate";
+            this.Text = "QR Create";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.imgToRender)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnRender;
+        private System.Windows.Forms.TextBox txtTextToUse;
+        private System.Windows.Forms.PictureBox imgToRender;
+        private System.Windows.Forms.ToolTip toolTip2;
+    }
+}
+
---
+++ frmQRCreate.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QRCreate
+{
+    public partial class QRCreate : Form
+    {
+        public QRCreate()
+        {
+            InitializeComponent();
+            FormText(false);
+        }
+
+        Bitmap theQrImage = null;
+        int pixelsPerBlock = 20;
+        int lastSQuare = -1;
+        string FormTextBase = "QR Create";
+
+        private void btnRender_Click(object sender, EventArgs e)
+        {
+            GenerateQR();
+            DisplayQRProperlySized();
+        }
+
+        private void GenerateQR()
+        {
+            if (!String.IsNullOrEmpty(txtTextToUse.Text))
+            {
+                QRCoder.QRCodeGenerator theQrGenerator = new QRCoder.QRCodeGenerator();
+                QRCoder.QRCodeData theQrCodeData = theQrGenerator.CreateQrCode(txtTextToUse.Text, QRCoder.QRCodeGenerator.ECCLevel.Q);
+                QRCoder.QRCode theQrCode = new QRCoder.QRCode(theQrCodeData);
+                theQrImage = theQrCode.GetGraphic(pixelsPerBlock);
+                imgToRender.Visible = true;
+                FormText(false);
+            }
+            else
+            {
+                imgToRender.Visible = false;
+            }
+        }
+
+        private void DisplayQRProperlySized()
+        {
+            if (theQrImage != null)
+            {
+                var thisSquare = Math.Min(imgToRender.Width, imgToRender.Height);
+                if (thisSquare > lastSQuare) { GenerateQR(); } // only need to regenerate when requiring more resolution...
+                theQrImage = ResizeImage(theQrImage, thisSquare, thisSquare);
+                imgToRender.Image = theQrImage;
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            DisplayQRProperlySized();
+        }
+
+        private void imgToRender_MouseClick(object sender, MouseEventArgs e)
+        {
+            Clipboard.SetImage(imgToRender.Image);
+            FormText(true);
+        }
+
+        private void FormText(bool Clipboardcopied)
+        {
+            this.Text = FormTextBase;
+            if (Clipboardcopied) { this.Text += "   (image copied to Windows Clipboard)"; }
+        }
+
+        // source:https://stackoverflow.com/questions/1922040/how-to-resize-an-image-c-sharp
+        /// <summary>
+        /// Resize the image to the specified width and height.
+        /// </summary>
+        /// <param name="image">The image to resize.</param>
+        /// <param name="width">The width to resize to.</param>
+        /// <param name="height">The height to resize to.</param>
+        /// <returns>The resized image.</returns>
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+            return destImage;
+        }
+    }
+}
